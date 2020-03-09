a8234c9ffef07fb37723c9efe05aaa42a33f8bb6
Miko <miko.palojarvi@student.oulu.fi>
2020-03-09 14:50:25 +0200

    WIP

--- cracker.py
+++ cracker.py
@@ -1,21 +1,50 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 from collections import Counter
 
-alphabet = 'abcdefghijklmnopqrstuvwxyzåäö'
-
 def main():
-	
-	file = open("message", "r")	
-	message = file.read()
-	file.close()
 
-	print '\n' + message + '\n'
-	freq = Counter(message)
-	print freq
+	encrypted_message = ""
+	with open("message", encoding="utf-8") as f:
+		for line in f:
+			encrypted_message += line
+
+	finnish_freq = ""
+	with open("finnishFreq", encoding="utf-8") as f:
+		for line in f:
+			finnish_freq += line
+
+	message_freq = Counter(encrypted_message)
+
+	key = ""
+	message = encrypted_message
+	quit = False;
+
+	while(quit==False):
+		print("\n" + message)
+		task = input("\nChange letter in format X=y, freq to print frequencies, \nquit to exit, reset to start over: ")
+		if len(task)==3:
+			oldchar = task[0]
+			newchar = task[2]
+			message = message.replace(oldchar,newchar)
+			key = key + (oldchar + "=" + newchar + ", ")
+		elif task=="freq":
+			print("\nFrequencies in message ")
+			print(message_freq)
+			print("\nFrequencies in finnish language " + finnish_freq)
+		elif task=="reset":
+			message = encrypted_message;
+		elif task=="quit":
+			quit = True;
+		else:
+			print("\nInvalid input")
+	
+	print("\nOriginal encrypted message:\n" + encrypted_message)
+	print("\nKeys for encryption: " + key)
+	print("\nDecrypted message:\n" + message)
+
 
 
 if __name__ == '__main__':
-    # Call the main function when this script is executed
-    main()
+	# Call the main function when this script is executed
+	main()
